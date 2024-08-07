64927f47f0b1e6a4e47b1aaff2202ee8dec38071
Roc7-gif <zosroc@gmail.com>
2024-08-07 07:20:49 +0100

    Update settings.py

--- app_z/settings.py
+++ app_z/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR / 'static']
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,"media/")
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles/")
+STATIC_FILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 """
 Profile
